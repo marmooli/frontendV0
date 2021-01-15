cc9dd080353a0c942c4417885e80cf6700f33002
Hamed Saffarian <hamed.saffarian@gmail.com>
2021-01-15 08:36:08 +0100

    massive debugged commas, locking, updates,...

--- src/components/TransferForm.vue
+++ src/components/TransferForm.vue
@@ -5,14 +5,18 @@
       <div class="column">
         <p>Trans Form</p>
         <div>
-          send:
+          <b v-if="!isLockedReceiveCur">Send:</b>
+          <span v-else>Send:</span>
           <input
             type="number"
             :disabled="validate ? '' : disabled"
             required="required"
             :min="fee"
+            step="0.01"
+            placeholder="enter send amount"
             v-model="send_cur"
-            @change="checkSend()"
+            @click="isLockedReceiveCur = false"
+            @select="isLockedReceiveCur = false"
           />
           <select
             name="send_cur"
@@ -25,22 +29,30 @@
             </option>
           </select>
         </div>
-        <p>- <b>{{ fee }}</b> {{send_selected}} transaction fees</p>
-        <p>= <b>{{ amountToEx }}</b> {{send_selected}} will exchanged with</p>
-        <p>your guaranteed rate: <b>{{ rate }}</b></p>
-        <p>(will be updated in <b>{{ countDown }}</b> sec.)</p>
+        <p>
+          - <b>{{ fee }}</b> {{ send_selected }} transaction fees
+        </p>
+        <p>
+          = <b>{{ amountToEx }}</b> {{ send_selected }} will exchanged with
+        </p>
+        <p>
+          your guaranteed rate: <b>{{ rate }}</b>
+        </p>
+        <p>
+          (will be updated in <b>{{ countDown }}</b> sec.)
+        </p>
         <div>
-          receive:
-          <button @click="isLockedReceiveCur = !isLockedReceiveCur">
-            {{ lockOrUnlock }}
-          </button>
-
+          <b v-if="isLockedReceiveCur">Receive:</b>
+          <span v-else>Receive:</span>
           <input
             type="number"
             :disabled="validate ? '' : disabled"
             min="0"
+            step=".01"
+            placeholder="enter receive amount"
             v-model="receive_cur"
-            @change="checkReceive()"
+            @click="isLockedReceiveCur = true"
+            @select="isLockedReceiveCur = true"
           />
           <select
             name="receive_cur"
@@ -71,8 +83,8 @@ const axios = require("axios").default;
 export default {
   data() {
     return {
-      send_cur: 100,
-      receive_cur: 0,
+      send_cur: "",
+      receive_cur: "",
       fee: 5.59,
       countDown: 12,
       rate: "updating...",
@@ -82,11 +94,6 @@ export default {
         3: { id: 3, val: "CAD" },
       },
       send_selected: "CAD",
-      // receive_curs: {
-      //   1: { id: 1, val: "AAA" },
-      //   2: { id: 2, val: "BBB" },
-      //   3: { id: 3, val: "CCC" },
-      // },
       receive_curs: {
         1: { id: 1, val: "EUR", b_rate: 1, s_rate: 0.1 },
         2: { id: 2, val: "USD", b_rate: 2, s_rate: 0.2 },
@@ -108,20 +115,21 @@ export default {
   },
   watch: {
     flagCount: "countDownTimer",
-    send_cur: "updateReceive",
-    receive_cur: "updateSend",
+    send_cur() {
+      // this.isLockedReceiveCur= false;
+      this.updateSendReceive();
+    },
+    receive_cur() {
+      // this.isLockedReceiveCur= true;
+      this.updateSendReceive();
+    },
     send_selected: "resetRate",
     receive_selected: "resetRate",
-    // send_cur: "checkSendCur",
-    // receive_cur: "checkReceiveCur",
-    // flagSendChanged: "updateReceive",
-    // flagReceiveChanged: "updateSend",
   },
 
   methods: {
     countDownTimer() {
       if (this.countDown < 1 || this.flagReset === true) {
-        // this.disabled = (this.disabled + 1) % 2;
         this.countDown = "-";
         this.rate = "updating...";
         setTimeout(() => {
@@ -129,30 +137,27 @@ export default {
           this.countDown = 12 + 1;
           this.flagReset = false;
         }, 1000);
-        // this.disabled = (this.disabled + 1) % 2;
       }
       setTimeout(() => {
         this.countDown -= 1;
         this.flagCount = !this.flagCount;
       }, 1000);
     },
+
     resetRate() {
       this.rate = "updating...";
       this.flagReset = true;
     },
+
     updateRate() {
       const max = 0.01;
       const min = -0.01;
       var rand_offset = Number((Math.random() * (max - min) + min).toFixed(4));
       console.log(rand_offset);
-      if (this.isLockedReceiveCur) {
-        this.updateSend();
-      } else {
-        this.updateReceive();
-      }
       let symbols = this.receive_selected;
       let base = this.send_selected;
       this.getRateAPI(base, symbols, rand_offset);
+      this.updateSendReceive();
     },
 
     getRateAPI(base, symbols, rand_offset) {
@@ -177,49 +182,42 @@ export default {
       }
     },
 
-    checkReceiveCur() {
-      this.flagSendChanged = true;
-    },
-
-    checkSend() {
-      this.send_cur = Number((this.send_cur * 1).toFixed(2));
-    },
-
-    checkReceive() {
-      this.receive_cur = Number((this.receive_cur * 1).toFixed(2));
-    },
-
-    updateReceive() {
+    updateSendReceive() {
       if (!this.isLockedReceiveCur) {
+        // update receive
         setTimeout(() => {
+          this.send_cur = Number((this.send_cur * 1).toFixed(2));
           this.receive_cur = Number(
             ((this.send_cur - this.fee) * this.rate).toFixed(2)
           );
           if (this.receive_cur < 0) {
             this.receive_cur = 0;
           }
-          this.flagSendChanged = false;
+        }, 500);
+      } else {
+        //update send
+        setTimeout(() => {
+          this.receive_cur = Number((this.receive_cur * 1).toFixed(2));
+          this.send_cur = Number(
+            (this.receive_cur / this.rate + this.fee).toFixed(2)
+          );
         }, 500);
       }
     },
-
-    updateSend() {
-      setTimeout(() => {
-        this.send_cur = Number(
-          (this.receive_cur / this.rate + this.fee).toFixed(2)
-        );
-        this.flagReceiveChanged = false;
-      }, 500);
-    },
   },
-
   computed: {
     validate() {
       return this.countDown === "-" || this.flagReset === true;
     },
+
     amountToEx() {
-      return Number((this.send_cur - this.fee).toFixed(2));
+      let amountToEx = Number((this.send_cur - this.fee).toFixed(2));
+      if (amountToEx < 0) {
+        amountToEx = 0;
+      }
+      return amountToEx;
     },
+
     lockOrUnlock() {
       let text = "U";
       if (this.isLockedReceiveCur) {
